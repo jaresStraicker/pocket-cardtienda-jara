f674e0c623ac6f209fb9ee14d3e8fb2d03c4fe58
Esteban Jara <108392903+jaresStraicker@users.noreply.github.com>
2025-01-30 10:21:59 -0300

    agregue router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import NavBar from './components/NavBar/NavBar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 
 const App = () => {
@@ -10,6 +11,13 @@ const App = () => {
     <>
     <NavBar/>
     <ItemListContainer cardTxt ={textoItemListContainer}/>
+    <Router>  
+  <Routes>
+    <Route path="/" element={<Home />} />
+      <Route path="/categorias/:categoriasId" element={<Category />} />
+      <Route path="/product/:productId" element={<ProductDetail />} />
+    </Routes>
+  </Router>
       
     </>
   )
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import './NavBar.css'
 import CartWigete from '../CartWidgete/CartWigete'
-
+import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <header>
         <h1>Pocket - Card -Tienda</h1>
         <nav>
             <ul>
-                <li>Inicio</li>
-                <li>Top Card</li>
-                <li>Scarle&Violet</li>
-                <li>Scarle&Violet - Paldea Evolved</li>
+                <li><Link to="/" />Inicio</li>
+                <li><Link to="/categorias"/>Top Card</li>
+                <li><Link to="/categorias"/>Scarle&Violet</li>
+                <li><Link to="/categorias"/>Scarle&Violet - Paldea Evolved</li>
             </ul>
             
         </nav>
